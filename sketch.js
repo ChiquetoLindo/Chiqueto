8cd32e5d90beecd1586a0c7eca37b947ac2ff55d
ChiquetoLindo <00001114816322sp@al.educacao.sp.gov.br>
2024-09-13 18:28:39 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,6 @@
+function setup() 
+{ createCanvas(400, 400); background("#000000") } 
+function draw() 
+{ stroke("#C936F4"); fill("#00FFCB");
+ if (mouseIsPressed) { rect(mouseX, mouseY, 20, 35);
+                     } }
